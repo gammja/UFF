07c055fdcd9e24bea73823ea5deb925f9d2f8f27
sp <some@gmail.com>
2015-09-16 02:03:46 +0600

    -

--- UFF/UFF.ps1
+++ UFF/UFF.ps1
@@ -1,4 +1,13 @@
-﻿#
-# Script.ps1
-#
+﻿$path = "C:\Users\sp\Source\Repos\ChangeTracking\Source"
 
+function IsIncluded($name)
+{
+  $exclude = "*\obj*", "*\bin*", "*\Properties", ".vs", "*\packages*"
+  $mathes = $exclude | Where-Object {$name -like $_}
+  $mathes.Count -eq 0
+}
+
+Get-ChildItem $path -File -Include *.cs, *.sln -Recurse `
+ | Where-Object {IsIncluded $_.FullName} `
+ | Format-Wide -Property Name -Column 1
+ #Select-Xml
