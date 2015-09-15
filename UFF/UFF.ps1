be3ab87b1cefa6fe38f9658d3bad2361cd47a1b4
sp <some@gmail.com>
2015-09-16 02:15:05 +0600

    -

--- UFF/UFF.ps1
+++ UFF/UFF.ps1
@@ -7,7 +7,11 @@ function IsIncluded($name)
   $mathes.Count -eq 0
 }
 
-Get-ChildItem $path -File -Include *.cs, *.sln -Recurse `
- | Where-Object {IsIncluded $_.FullName} `
- | Format-Wide -Property Name -Column 1
+$ns = @{msb = 'http://schemas.microsoft.com/developer/msbuild/2003'}
+Get-ChildItem $path -File -Include *.sln -Recurse `
+  | Select-Xml '/Project/ItemGroup[2]//Compile[18]@Include' -Namespace $ns
+
+#Get-ChildItem $path -File -Include *.cs, *.sln -Recurse `
+# | Where-Object {IsIncluded $_.FullName} `
+# | Format-Wide -Property Name -Column 1
  #Select-Xml
